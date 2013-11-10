769b8139ad1956002c079c590ae2d9dd7777174e
Lenny Basuino <lenny@redmine.(none)>
2013-11-10 11:14:56 -0500

    Initial beta commit

---
+++ el_rte/init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :el_rte do
+  name 'El Rte plugin'
+  author 'Lenny Basuino'
+  description 'Uses elRTE to edit wiki pages'
+  version '0.0.1'
+  url ''
+  author_url 'http://www.basuinosolutionsllc.com/about'
+end
